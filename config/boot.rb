8db5d996b2bb513f322bc77a6db9cc940b2bdada
Alan Alvarado <alan.alvespi@gmail.com>
2012-08-27 16:42:25 -0500

    yaml

--- config/boot.rb
+++ config/boot.rb
@@ -1,7 +1,7 @@
-require 'rubygems'
-#Solving mysql conenct parse
 require 'yaml'
 YAML::ENGINE.yamler= 'syck'
+require 'rubygems'
+
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
