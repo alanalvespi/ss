c7528b7e8417ee080ae9ac0daabbff770267acce
Jerry Westrick <jerry@westrick.com>
2012-08-30 13:47:14 -0500

    include example of directory building

--- feeders/market_update/test.rb
+++ feeders/market_update/test.rb
@@ -1,3 +1,8 @@
+
+
+
+
+
 #
 # test run program from ruby
 #
@@ -14,10 +19,32 @@ host     = 'ec2-23-23-212-26.compute-1.amazonaws.com'
 port     = '3306'
 #  logfile=#{logfile} 
 
-result = system("bundle exec ruby -C#{execdir} #{pgm} valuta=#{valuta} database=#{database} username=#{user} password=#{password} host=#{host} port=#{port}")
+#result = system("bundle exec ruby -C#{execdir} #{pgm} valuta=#{valuta} database=#{database} username=#{user} password=#{password} host=#{host} port=#{port}")
 
-if result then
-  puts "successfully executed #{pgm}"
-else
-  puts "execution of #{pgm} returned error"
+#if result then
+#  puts "successfully executed #{pgm}"
+#else
+#  puts "execution of #{pgm} returned error"
+#end
+
+
+require 'find'
+dir = 'data'
+data = {}
+Find.find(dir) do |path|
+  #p "\n>>#{path}::"
+  parts = path.split('/')
+  t = data
+  # travel/make directory structures 
+  parts[0..-2].each { |p|
+    next unless p
+    #p "[#{p}]" 
+    t[p] = {} unless t.has_key?(p) 
+    t = t[p]
+  }
+
+  t[parts[-1]] = path unless FileTest.directory?(path)
 end
+
+
+p data
