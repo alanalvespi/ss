088b9b56a01a6135b2944598a1cba014cb472fcc
Alan Alvarado <alan.alvespi@gmail.com>
2012-08-30 22:18:28 -0500

    test case

--- test/functional/feeder_controller_test.rb
+++ test/functional/feeder_controller_test.rb
@@ -4,4 +4,13 @@ class FeederControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "should have # of rows in the xml" do
+    #here your object to test, i.e. 
+    # feeder.xml        
+    some_undefined_variable
+    #assertion case in the xml, i.e.
+    # assert
+    assert true   
+  end
 end
