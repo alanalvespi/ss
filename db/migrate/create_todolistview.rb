8722715b15322d0ed38815b2733d250a1a0a2d22
unknown <jerry@Huevon.(none)>
2012-10-02 01:25:07 -0500

    change create view

--- db/migrate/create_todolistview.rb
+++ db/migrate/create_todolistview.rb
@@ -1,7 +1,7 @@
 class CreateTodoListView < ActiveRecord::Migration
     def self.up
       sql = <<-EOS
-        CREATE VIEW todolist
+CREATE VIEW todolist
         (
         tablename,
         id,
@@ -11,64 +11,64 @@ class CreateTodoListView < ActiveRecord::Migration
         )
         AS
         select 
-          'clients' AS 'tablename',
-          'clients'.'client_id' AS 'id',
-          'clients'.'client_name' AS 'description',
-          'clients'.'state' AS 'state',
-          'clients'.'reason' AS 'reason' 
-        from 'clients' 
+          'clients' AS tablename,
+          clients.client_id AS id,
+          clients.client_name AS description,
+          clients.state AS state,
+          clients.reason AS reason 
+        from clients 
         where
-          ('clients'.'state' <> 0) 
+          (clients.state <> 0) 
         union
         select 
-          'markets' AS 'tablename',
-          'markets'.'market_id' AS 'id',
-          'markets'.'market_friendly_name' AS 'description',
-          'markets'.'state' AS 'state',
-          'markets'.'reason' AS 'reason' 
-        from 'markets' 
+          'markets' AS tablename,
+          markets.market_id AS id,
+          markets.market_friendly_name AS description,
+          markets.state AS state,
+          markets.reason AS reason 
+        from markets 
         where
-          ('markets'.'state' <> 0) 
+          (markets.state <> 0) 
         union
         select 
-          'plantypefunds' AS 'tablename',
-          'plantypefunds'.'fund_id' AS 'id',
-          'plantypefunds'.'fund_name' AS 'description',
-          'plantypefunds'.'state' AS 'state',
-          'plantypefunds'.'reason' AS 'reason' 
-        from 'plantypefunds' 
+          'plantypefunds' AS tablename,
+          plantypefunds.fund_id AS id,
+          plantypefunds.fund_name AS description,
+          plantypefunds.state AS state,
+          plantypefunds.reason AS reason 
+        from plantypefunds 
         where
-          ('plantypefunds'.'state' <> 0) 
+          (plantypefunds.state <> 0) 
         union
         select 
-          'plantypes' AS 'tablename',
-          'plantypes'.'plantype_id' AS 'id',
-          'plantypes'.'plantype_name' AS 'description',
-          'plantypes'.'state' AS 'state',
-          'plantypes'.'reason' AS 'reason' 
-        from 'plantypes' 
+          'plantypes' AS tablename,
+          plantypes.plantype_id AS id,
+          plantypes.plantype_name AS description,
+          plantypes.state AS state,
+          plantypes.reason AS reason 
+        from plantypes 
         where
-          ('plantypes'.'state' <> 0) 
+          (plantypes.state <> 0) 
         union
         select 
-          'policies' AS 'tablename',
-          'policies'.'policy_id' AS 'id',
-          'policies'.'policy_number' AS 'description',
-          'policies'.'state' AS 'state',
-          'policies'.'reason' AS 'reason' 
-        from 'policies' 
+          'policies' AS tablename,
+          policies.policy_id AS id,
+          policies.policy_number AS description,
+          policies.state AS state,
+          policies.reason AS reason 
+        from policies 
         where
-          ('policies'.'state' <> 0) 
+          (policies.state <> 0) 
         union
         select 
-          'policyfunds' AS 'tablename',
-          'policyfunds'.'policyfund_id' AS 'id',
-          concat('policyfunds'.'policy_id',':','policyfunds'.'fund_id') AS 'description',
-          'policyfunds'.'state' AS 'state',
-          'policyfunds'.'reason' AS 'reason' 
-        from 'policyfunds' 
+          'policyfunds' AS tablename,
+          policyfunds.policyfund_id AS id,
+          concat(policyfunds.policy_id,:,policyfunds.fund_id) AS description,
+          policyfunds.state AS state,
+          policyfunds.reason AS reason 
+        from policyfunds 
         where
-          ('policyfunds'.'state' <> 0);
+          (policyfunds.state <> 0);
       EOS
     
       execute sql
