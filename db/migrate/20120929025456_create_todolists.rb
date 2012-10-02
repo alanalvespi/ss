fdfa306f06f7a64f57f867eb18c1b3628052aa80
Alan Alvarado <alan.alvespi@gmail.com>
2012-10-02 01:30:18 -0500

    update test

--- db/migrate/20120929025456_create_todolists.rb
+++ db/migrate/20120929025456_create_todolists.rb
@@ -10,10 +10,9 @@ class CreateTodolists < ActiveRecord::Migration
   #    t.timestamps
   #  end
   #end
-  
   def self.up
-      sql = 
-      "CREATE VIEW todolist
+    sql =
+    "CREATE VIEW todolist
         (
         tablename,
         id,
@@ -22,71 +21,71 @@ class CreateTodolists < ActiveRecord::Migration
         reason
         )
         AS
-        select 
+        select
           'clients' AS tablename,
           clients.client_id AS id,
           clients.client_name AS description,
           clients.state AS state,
-          clients.reason AS reason 
-        from clients 
+          clients.reason AS reason
+        from clients
         where
-          (clients.state <> 0) 
+          (clients.state <> 0)
         union
-        select 
+        select
           'markets' AS tablename,
           markets.market_id AS id,
           markets.market_friendly_name AS description,
           markets.state AS state,
-          markets.reason AS reason 
-        from markets 
+          markets.reason AS reason
+        from markets
         where
-          (markets.state <> 0) 
+          (markets.state <> 0)
         union
-        select 
+        select
           'plantypefunds' AS tablename,
           plantypefunds.fund_id AS id,
           plantypefunds.fund_name AS description,
           plantypefunds.state AS state,
-          plantypefunds.reason AS reason 
-        from plantypefunds 
+          plantypefunds.reason AS reason
+        from plantypefunds
         where
-          (plantypefunds.state <> 0) 
+          (plantypefunds.state <> 0)
         union
-        select 
+        select
           'plantypes' AS tablename,
           plantypes.plantype_id AS id,
           plantypes.plantype_name AS description,
           plantypes.state AS state,
-          plantypes.reason AS reason 
-        from plantypes 
+          plantypes.reason AS reason
+        from plantypes
         where
-          (plantypes.state <> 0) 
+          (plantypes.state <> 0)
         union
-        select 
+        select
           'policies' AS tablename,
           policies.policy_id AS id,
           policies.policy_number AS description,
           policies.state AS state,
-          policies.reason AS reason 
-        from policies 
+          policies.reason AS reason
+        from policies
         where
-          (policies.state <> 0) 
+          (policies.state <> 0)
         union
-        select 
+        select
           'policyfunds' AS tablename,
           policyfunds.policyfund_id AS id,
           concat(policyfunds.policy_id,:,policyfunds.fund_id) AS description,
           policyfunds.state AS state,
-          policyfunds.reason AS reason 
-        from policyfunds 
+          policyfunds.reason AS reason
+        from policyfunds
         where
           (policyfunds.state <> 0);"
-          
-      execute(sql)
-    end
- 
-    def self.down
-      sql = "DROP VIEW todolist"
-      execute(sql)
-    end
+
+    execute(sql)
+  end
+
+  def self.down
+    sql = "DROP VIEW todolist"
+    execute(sql)
+  end
 end
