310c5a241b51ef2044c20a0018fb5b4683a8bc0e
Alan Alvarado <alan.alvespi@gmail.com>
2012-10-02 01:13:45 -0500

    todolist update view

--- db/migrate/20120929025456_create_todolists.rb
+++ db/migrate/20120929025456_create_todolists.rb
@@ -13,7 +13,7 @@ class CreateTodolists < ActiveRecord::Migration
   
   def self.up
       sql = 
-        "CREATE VIEW todolist
+      "CREATE VIEW todolist
         (
         tablename,
         id,
@@ -23,66 +23,65 @@ class CreateTodolists < ActiveRecord::Migration
         )
         AS
         select 
-          'clients' AS 'tablename',
-          'clients'.'client_id' AS 'id',
-          'clients'.'client_name' AS 'description',
-          'clients'.'state' AS 'state',
-          'clients'.'reason' AS 'reason' 
-        from 'clients' 
+          'clients' AS tablename,
+          clients.client_id AS id,
+          clients.client_name AS description,
+          clients.state AS state,
+          clients.reason AS reason 
+        from clients 
         where
-          ('clients'.'state' <> 0) 
+          (clients.state <> 0) 
         union
         select 
-          'markets' AS 'tablename',
-          'markets'.'market_id' AS 'id',
-          'markets'.'market_friendly_name' AS 'description',
-          'markets'.'state' AS 'state',
-          'markets'.'reason' AS 'reason' 
-        from 'markets' 
+          'markets' AS tablename,
+          markets.market_id AS id,
+          markets.market_friendly_name AS description,
+          markets.state AS state,
+          markets.reason AS reason 
+        from markets 
         where
-          ('markets'.'state' <> 0) 
+          (markets.state <> 0) 
         union
         select 
-          'plantypefunds' AS 'tablename',
-          'plantypefunds'.'fund_id' AS 'id',
-          'plantypefunds'.'fund_name' AS 'description',
-          'plantypefunds'.'state' AS 'state',
-          'plantypefunds'.'reason' AS 'reason' 
-        from 'plantypefunds' 
+          'plantypefunds' AS tablename,
+          plantypefunds.fund_id AS id,
+          plantypefunds.fund_name AS description,
+          plantypefunds.state AS state,
+          plantypefunds.reason AS reason 
+        from plantypefunds 
         where
-          ('plantypefunds'.'state' <> 0) 
+          (plantypefunds.state <> 0) 
         union
         select 
-          'plantypes' AS 'tablename',
-          'plantypes'.'plantype_id' AS 'id',
-          'plantypes'.'plantype_name' AS 'description',
-          'plantypes'.'state' AS 'state',
-          'plantypes'.'reason' AS 'reason' 
-        from 'plantypes' 
+          'plantypes' AS tablename,
+          plantypes.plantype_id AS id,
+          plantypes.plantype_name AS description,
+          plantypes.state AS state,
+          plantypes.reason AS reason 
+        from plantypes 
         where
-          ('plantypes'.'state' <> 0) 
+          (plantypes.state <> 0) 
         union
         select 
-          'policies' AS 'tablename',
-          'policies'.'policy_id' AS 'id',
-          'policies'.'policy_number' AS 'description',
-          'policies'.'state' AS 'state',
-          'policies'.'reason' AS 'reason' 
-        from 'policies' 
+          'policies' AS tablename,
+          policies.policy_id AS id,
+          policies.policy_number AS description,
+          policies.state AS state,
+          policies.reason AS reason 
+        from policies 
         where
-          ('policies'.'state' <> 0) 
+          (policies.state <> 0) 
         union
         select 
-          'policyfunds' AS 'tablename',
-          'policyfunds'.'policyfund_id' AS 'id',
-          concat('policyfunds'.'policy_id',':','policyfunds'.'fund_id') AS 'description',
-          'policyfunds'.'state' AS 'state',
-          'policyfunds'.'reason' AS 'reason' 
-        from 'policyfunds' 
+          'policyfunds' AS tablename,
+          policyfunds.policyfund_id AS id,
+          concat(policyfunds.policy_id,:,policyfunds.fund_id) AS description,
+          policyfunds.state AS state,
+          policyfunds.reason AS reason 
+        from policyfunds 
         where
-          ('policyfunds'.'state' <> 0);"
-      
-    
+          (policyfunds.state <> 0);"
+          
       execute(sql)
     end
  
